2b1ff3386580f388a25d6e7fd2c302787a3a7b6a
Vijay Tholpadi <vijay@thegeekprojekt.com>
2024-03-15 17:48:56 +0800

    Adds SPM support

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.9
+import PackageDescription
+let package = Package(
+    name: "SSPullToRefresh",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "SSPullToRefresh",
+            targets: ["SSPullToRefresh"])
+    ],
+    targets: [
+        .target(
+            name: "SSPullToRefresh",
+            path: "SSPullToRefresh",
+            publicHeadersPath: "."
+        )
+    ]
+)
