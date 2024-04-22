fa31fb5cfcde0287cd674872cafccf32a8e4f344
Vijay Tholpadi <vijay@thegeekprojekt.com>
2024-04-22 14:06:15 +0800

    Moves header back into the root folder

---
+++ .build/arm64-apple-macosx/debug/SSPullToRefresh.build/SSPullToRefreshSimpleContentView.m.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Applications/Xcode-15.3.0.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.4.sdk/usr/include/module.modulemap \
+  /Applications/Xcode-15.3.0.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.4.sdk/usr/include/mach-o/module.modulemap \
+  /Users/vijaytholpadi/Repositories/platform/SSPullToRefresh/SSPullToRefresh/SSPullToRefreshSimpleContentView.m \
+  /Users/vijaytholpadi/Repositories/platform/SSPullToRefresh/SSPullToRefresh/include/SSPullToRefreshSimpleContentView.h \
+  /Users/vijaytholpadi/Repositories/platform/SSPullToRefresh/SSPullToRefresh/include/SSPullToRefreshView.h
--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             name: "SSPullToRefresh",
             path: "SSPullToRefresh",
             exclude: ["Info.plist"],
-            publicHeadersPath: "include"
+            publicHeadersPath: "."
         )
     ]
 )
